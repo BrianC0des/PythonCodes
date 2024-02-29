94048b99963788ac8df5053886f4ff3f11cf5bb1
BriCode <bryansaavedrab272@gmail.com>
2024-02-29 10:26:55 +0800

    probability.py

--- probability.py
+++ probability.py
@@ -1,9 +1,13 @@
-x = [483,651,495,567,690,265]
+x = [483, 651, 495, 567, 690, 265]
+y = []
 
+# Normalize values in x
+total_sum = sum(x)
 for i in x:
-    z =  i/sum(x)
-    k = abs(z)
-    dr1 = ("{0:.2f}".format(z))
-    print(dr1)
-    
-    
+    normalized_value = i / total_sum
+    y.append(normalized_value)
+
+print("Original list x:", x)
+print("Normalized list y:", y)
+print("Sum of normalized values:", sum(y))
+print("Sum of original values:", total_sum)
